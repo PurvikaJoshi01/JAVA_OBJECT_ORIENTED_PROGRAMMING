9f4c4a0e9db92bdaf14e0f0b4c03d6a621ad9729
PurvikaJoshi01 <113506230+PurvikaJoshi01@users.noreply.github.com>
2024-02-02 13:14:40 +0530

    Add files via upload

---
+++ practice.java
@@ -0,0 +1,10 @@
+package packone;
+public class practice{
+	public void display() {
+		System.out.println("Trying to import this package");
+	}
+	public static void main(String[]args) {
+		practice p=new practice();
+		p.display();
+	}
+}
